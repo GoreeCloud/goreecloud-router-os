eed3a01943e28d02de7abab0bcd6c14a62d9c133
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-09-10 13:08:54 -0500

    Export journal and recovery APIs

--- prototype/routeros_m0/__init__.py
+++ prototype/routeros_m0/__init__.py
@@ -1,17 +1,25 @@
 """Milestone 0 unprivileged Router OS architecture prototype."""
 
 from .compiler import compile_plan
+from .journal import AtomicJournalStore, JournalError, JournalRecord
 from .model import ConfigRevision, normalize_config
+from .recovery import RecoveryRequired, RecoveryResult, recover_interrupted_transaction
 from .transaction import InMemoryRuntime, TransactionResult, apply_transaction
 from .validator import ValidationError, validate_config
 
 __all__ = [
+    "AtomicJournalStore",
     "ConfigRevision",
     "InMemoryRuntime",
+    "JournalError",
+    "JournalRecord",
+    "RecoveryRequired",
+    "RecoveryResult",
     "TransactionResult",
     "ValidationError",
     "apply_transaction",
     "compile_plan",
     "normalize_config",
+    "recover_interrupted_transaction",
     "validate_config",
 ]
